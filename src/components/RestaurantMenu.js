e3121866f23f49ae616738f47e4f2a83412a6cea
Preeti <preeti21jly@gmail.com>
2024-05-29 08:41:20 +0530

    yet to link rest cards

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,31 +1,44 @@
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
+import { MENU_API } from "../../utils/constants";
 const RestaurantMenu = () => {
   const [resInfo, setResInfo] = useState(null);
+
+  const { resId } = useParams();
   useEffect(() => {
     fetchMenu();
   }, []);
   const fetchMenu = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.600828&lng=77.446917&restaurantId=53253&catalog_qa=undefined&submitAction=ENTER"
-    );
+    const data = await fetch(MENU_API + resId);
     const json = await data.json();
 
     console.log(json);
     setResInfo(json.data);
   };
-  //const {name, cuisines, costForTwoMessage} = resInfo?.cards[2]?.card?.card?.info?;
 
-  return resInfo === null ? (
-    <Shimmer />
-  ) : (
+  if (resInfo === null) return <Shimmer />;
+
+  const { name, cuisines, costForTwoMessage } =
+    resInfo?.cards[2]?.card?.card?.info;
+
+  const { itemCards } =
+    resInfo?.cards[4].groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+  console.log(itemCards);
+
+  return (
     <div className="menu">
-      <h1>{resInfo?.cards[2]?.card?.card?.info?.name}</h1>
-      <h2>Menu</h2>
+      <h1>{name}</h1>
+      <p>
+        {cuisines.join(", ")} -{costForTwoMessage}
+      </p>
+      <h1>Menu</h1>
       <ul>
-        <li>Biryani</li>
-        <li>Burger</li>
-        <li>Diet Coke</li>
+        {itemCards.map((item) => (
+          <li key={item.card.info.id}>
+            {item.card.info.name} {" -        Rs"} {item.card.info.price / 100}
+          </li>
+        ))}
       </ul>
     </div>
   );
--- utils/constants.js
+++ utils/constants.js
@@ -2,5 +2,6 @@ export const LOGO_URL =
   "https://seeklogo.com/images/F/food-logo-59E5A73AFD-seeklogo.com.png";
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-
-// to export multiple thongs...use Named export'
+export const MENU_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.600828&lng=77.446917&";
+// to export multiple things...use Named export'
